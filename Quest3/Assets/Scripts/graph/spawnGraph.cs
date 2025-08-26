e1f8c79c21fecd78526d64facca548ed397e7b04
sekarshada <shadag@student.unimelb.edu.au>
2025-08-26 14:58:37 +1000

    fix graph in static position

--- Quest3/Assets/Scripts/graph/spawnGraph.cs
+++ Quest3/Assets/Scripts/graph/spawnGraph.cs
@@ -25,22 +25,17 @@ public class spawnGraph : MonoBehaviour
     {
         // Position the canvas in front of the camera
         target = Camera2Follow.transform;
-    }
 
-    void Update()
-    {
-        // Define my target position in front of the camera with a downward offset
+        // Position the graph in front of the camera with a downward offset
+        target = Camera2Follow.transform;
         Vector3 targetPosition = target.TransformPoint(new Vector3(0, -downwardOffset, CameraDistance));
+        transform.position = targetPosition;
 
-        // Smoothly move my object towards that position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-
-        // Version 1: my object's rotation is always facing the camera with no dampening
-        transform.LookAt(transform.position + Camera2Follow.transform.rotation * Vector3.forward, Camera2Follow.transform.rotation * Vector3.up);
-
+        // Make the graph face the same direction as the camera
+        transform.rotation = Camera2Follow.transform.rotation;
         transform.rotation *= Quaternion.Euler(0, -90, 0);
-        // Version 2: my object's rotation isn't finished synchronously with the position smooth damp
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, 35 * Time.deltaTime * smoothTime);
+            
+   
     }
 
 
